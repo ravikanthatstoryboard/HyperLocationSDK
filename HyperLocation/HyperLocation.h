b7838a700fde87c393a68361d96bbff241b2cc89
Ravikanth Macbook Pro <ravikanth@storyboard.co>
2018-07-26 11:02:57 +0530

    first commit

---
+++ HyperLocation/HyperLocation.h
@@ -0,0 +1,19 @@
+//
+//  HyperLocation.h
+//  HyperLocation
+//
+//  Created by GeoSpark Mac #1 on 26/07/18.
+//  Copyright © 2018 GeoSpark, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HyperLocation.
+FOUNDATION_EXPORT double HyperLocationVersionNumber;
+
+//! Project version string for HyperLocation.
+FOUNDATION_EXPORT const unsigned char HyperLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HyperLocation/PublicHeader.h>
+
+
---
+++ HyperLocation/HyperLocation.swift
@@ -0,0 +1,16 @@
+//
+//  HyperLocation.swift
+//  HyperLocation
+//
+//  Created by GeoSpark Mac #1 on 26/07/18.
+//  Copyright © 2018 GeoSpark, Inc. All rights reserved.
+//
+
+import UIKit
+
+class HyperLocation: NSObject {
+
+    public func test(){
+        print("HyperLocation")
+    }
+}
